9c10895ab61f61ce4d2f8f0ecd69707d73109354
Liv Cella <47287116+LivInAbsurdism@users.noreply.github.com>
2019-11-13 15:52:05 -0700

    fixing commit issues

--- app.js
+++ app.js
@@ -6,5 +6,27 @@ let kslUrl = 'https://cars.ksl.com/';
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 926});
   await page.goto(kslUrl);
-}
-  )
+
+  // get car details
+  let carData = await page.evaluate(() => {
+    let cars = [];
+    // get car elements
+    let carsElms = document.querySelectorAll('div.listing[data-listing]');
+    // get car data
+    carsElms.forEach((carelement) => {
+      let carJson = {};
+      try {
+        carJson.name = carelement.querySelector('a.link').innerText;
+        carJson.price = carelement.querySelector('div.listing-detail-line.price').innerText;
+        carJson.mileage = carelement.querySelector('div.listing-detail-line.mileage').innerText;
+      }
+    catch (exception){
+
+    }
+    cars.push(carJson);
+  });
+  return cars;
+});
+
+console.dir(carData);
+})();
